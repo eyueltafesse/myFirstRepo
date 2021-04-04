b7149acbeae252fecad64294a66e155c77219a2a
Eyuel <etafesse@gmail.com>
2021-04-03 20:49:03 -0500

    Create decoratemytext.js

---
+++ decoratemytext/decoratemytext.js
@@ -0,0 +1,29 @@
+
+
+window.onload=function() {
+    var bigger = document.getElementById("bd");
+    bigger.onclick = function changeSize() {
+        let textArea=document.getElementById("txt1");
+        var fontSize= window.getComputedStyle(textArea,null).fontSize;
+        console.log(fontSize);
+        
+            var resized=parseInt(fontSize)+ 2;
+        
+           textArea.style.fontSize=resized+"px";
+    }
+
+        let bling=document.getElementById("bling");
+        bling.onchange=modify;
+        function modify(){
+            
+            let modified=document.getElementById("txt1");
+            if(this.checked){
+            modified.style.fontWeight="bold"
+            modified.style.color="green"
+            modified.style.textDecoration="underline"
+            }else{
+                modified.style="normal"
+            }
+        }
+       
+}
